eb000e1f5081e6fd461db147dc6372472d03bb78
Karly <Karly@hungryhoffkitchen.attlocal.net>
2016-09-18 16:51:49 -0500

    created game board html

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,11 +3,29 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/* Comment out If statement
 if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardThree === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
+} */
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function () {
+	for (var i = 0 ; i < 4 ; i++) {
+		var newDiv = document.createElement('div');
+		//Make class of newDiv "card"
+		newDiv.setAttribute('class', 'card');
+		//Or is it this? newDiv.className = 'card';
+		newDiv.appendChild(gameBoard);
+	}
 }
+
+createBoard();
+
+
+
 
