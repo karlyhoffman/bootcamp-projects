343f3b3cc38623a8f694d7febfa8544f6397bc8d
Karly <Karly@hungryhoffkitchen.attlocal.net>
2016-09-18 20:04:03 -0500

    the final game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,13 @@
-var cardOne = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+/* var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king"; 
 
-/* Comment out If statement
+Comment out If statement
 if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardThree === cardFour) {
@@ -12,20 +16,47 @@ if (cardOne === cardTwo) {
 	alert("Sorry, try again.");
 } */
 
-var gameBoard = document.getElementById('game-board');
+var board = document.getElementById('game-board');
 
-var createBoard = function () {
-	for (var i = 0 ; i < 4 ; i++) {
-		var newDiv = document.createElement('div');
+var createBoard = function() {
+
+	for (var i = 0 ; i < cards.length ; i++) {
+		var cardElement = document.createElement('div');
 		//Make class of newDiv "card"
-		newDiv.setAttribute('class', 'card');
-		//Or is it this? newDiv.className = 'card';
-		newDiv.appendChild(gameBoard);
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
+		board.appendChild(cardElement);
 	}
-}
+};
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://pixabay.com/get/e835b9062afd083ed1584d05fb0938c9bd22ffd41db6154391f6c77ba7/elvis-1089299_1280.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='http://orig07.deviantart.net/977e/f/2014/253/a/7/beyonce_color_mini_by_nottiart-d7ynyft.jpg'>"; //queen
+	}
+
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+};
+
+
+var isMatch = function(cards) {
+	if (cards[0] === cards[1]) {
+		alert("Is your name Cupid? Because you found a match!")
+	} else {
+		alert("Sorry, give it another try.")
+	}
+};
 
 createBoard();
-
-
-
 
