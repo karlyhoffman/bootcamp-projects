69e0d433cdb98dc5a259f224622473507f4b2d1c
Karly <Karly@HungryHoffKitchen.local>
2016-10-11 18:22:09 -0500

    organized horse constructor

--- scripts/app.js
+++ scripts/app.js
@@ -3,64 +3,82 @@ console.log('Linked');
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+//James wants you to change the horse styling everytime
+// UNICORN!!! "Use Glow Property"
+	//Pops up on last level OR Easter Egg = happen to click horses in a certain order
+
+var score = 0;
+var level = 1;
+var lives = 3;   // Try to have icons to represent each life
+
 var horses = [];
 
-function Horse(addHorse, switchDirection) {
-	//James wants you to change the horse styling everytime
-	// UNICORN!!! "Use Glow Property"
-		//Pops up on last level OR Easter Egg -- like if you click the horses in a certain order
+var img = new Image();
+img.src = "images/evenSmallerHorse.png";
 
-	var xCoor = Math.floor(Math.random() * 370) + 90; // Random x coordinate = # from 90 to 460 
-	var yCoor = Math.floor(Math.random() * 160) + 90; // Random y coordinate = # from 90 to 250
+canvas.addEventListener('click', function(e){
+	console.log(e.pageX);
+	console.log(e.pageY);
 
-	this.addHorse = function() {
-		var img = new Image();
-		// img.onload = function(){
-		// 	ctx.drawImage(img,xCoor,yCoor);
-		// };
-		img.onload = moveHorse;
-		img.src = "images/evenSmallerHorse.png";   // load image
-		horses.push(this);
+	// if (  (horseOne.currentXCoor() - 20) <= e.pageX <= (horseOne.currentXCoor() + 20) ) {
+	// 		console.log("Horse clicked!");
+	// };
 
-		function moveHorse() {
-		  ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
-		  ctx.drawImage(img, xCoor,yCoor);   // draw image at current position
-		  xCoor += 2;
-		  yCoor += -2;
-		  if (xCoor < canvas.width) requestAnimationFrame(moveHorse);    // loop
-		};
-	};
+});
 
-	this.switchDirection = function(){
-		// this.on('click', function(){
-		
-		// });
-	};
 
+function horseImageDraw(horse){
+	img.onload = horse.animate;
 };
 
-// Start with two horses
+function Horse() {
+	horses.push(this);
+	var base = this;
+	this.xCoor = Math.floor(Math.random() * 370) + 90, 
+	this.yCoor = Math.floor(Math.random() * 160) + 90, 
+
+	this.randomXDirection = (Math.random() * 2 - 1) * 2, 
+	this.randomYDirection = (Math.random() * 2 - 1) * 2,
+
+	this.moveHorse = function() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);  
+		// base.xCoor += base.randomXDirection;
+		// base.yCoor += base.randomYDirection;
+		ctx.drawImage(img, base.xCoor, base.yCoor, 50, 50);
+		// console.log(base.xCoor, base.yCoor)
+ 	},
+
+ 	this.animate = function(){
+		base.moveHorse();
+		// animate if within canvas borders:
+		if (base.xCoor < canvas.width && base.yCoor < canvas.height
+			&& base.yCoor > -60 && base.xCoor > -60) {
+			window.requestAnimationFrame(base.animate);
+		};
+	},
+
+	this.currentXCoor = function(){
+		return base.xCoor;
+	},
+
+	this.currentYCoor = function(){
+		return base.yCoor;
+	}
+};
+
+
 var horseOne = new Horse();
-var horseTwo = new Horse();
+// var horseTwo = new Horse();
 
-horseOne.addHorse();
-horseTwo.addHorse();
-
-var horseThree = new Horse(); // Level 2
-var horseFour = new Horse(); // Level 3
-var horseFive = new Horse(); // Level 4
-var horseSix = new Horse(); // Level 5
-var horseSeven = new Horse(); // Level 6
-var horseEight = new Horse(); // Level 7
-
-// horseThree.addHorse();
-// horseFour.addHorse();
-// horseFive.addHorse();
-// horseSix.addHorse();
-// horseSeven.addHorse();
-// horseEight.addHorse();
+horseImageDraw(horseOne);
+// horseImageDraw(horseTwo);
 
 
-
+// var horseThree = new Horse();  // Level 2
+// var horseFour = new Horse();  // Level 3
+// var horseFive = new Horse();  // Level 4
+// var horseSix = new Horse();  // Level 5
+// var horseSeven = new Horse();  // Level 6
+// var horseEight = new Horse();  // Level 7
 
 
