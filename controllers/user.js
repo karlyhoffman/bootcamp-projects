ed04b1d6a31ab6c3b0a22204eb7674c9e7df4f79
Karly <Karly@HungryHoffKitchen.local>
2016-10-19 12:24:36 -0500

    updated user ctrl

--- controllers/user.js
+++ controllers/user.js
@@ -22,4 +22,16 @@ ctrl.get('/', function(req, res, next){
 	// res.render('view', {})
 });
 
+ctrl.get('/1', function(req, res, next) {
+	res.json(users[0]);
+});
+
+ctrl.get('/2', function(req, res, next) {
+	res.json(users[1]);
+});
+
+ctrl.get('/3', function(req, res, next) {
+	res.json(users[2]);
+});
+
 module.exports = ctrl;
