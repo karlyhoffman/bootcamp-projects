0db20a6bbc04c1b3308d37860452a39c264edc21
Karly <Karly@hungryhoffkitchen.attlocal.net>
2016-10-20 21:50:28 -0500

    one.js complete

---
+++ one.js
@@ -0,0 +1,14 @@
+// Data Types
+
+var pi = 3.14; // number
+
+var fullName = 'Karly Lynn Hoffman'; // string
+
+var primeNumbers = ['2', '3', '5', '7', '11', '13', '17', '19']; // object
+
+var myAttributes = {}; // object
+myAttributes['age'] = 27;
+myAttributes['city'] = 'Chicago';
+myAttributes['favoriteBeer'] = 'Rogue Dead Guy Ale';
+
+var wdi = true;	// boolean
