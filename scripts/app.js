f0524976662b19f3afa1173fb10e380a0cd81d69
Karly Hoffman <karlyhoffman@gmail.com>
2016-12-08 10:33:32 -0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,7 @@
-console.log("Cool, the window loaded.")
+console.log("Linked.")
 
 
 //ROUND 1
-
 // declare function named "lengths"
 // that accepts a arguments named "arrayOfStrings"
 function length(arrayOfStrings){
@@ -16,17 +15,12 @@ function length(arrayOfStrings){
 		//push string length to new array
 		stringLengthArray.push(arrayOfStrings[i]);
 	}
-
 	return stringLengthArray;
 };
-
 console.log(length(["Karly", "Lynn", "Hoffman"]));
 
 
-
-
 // ROUND 2
-
 // declare function named "transmogrifier" with three number arguments
 // transmogrifier returns product of 1st two numbers, raised to the power of third number
 function transmogrifier(num1, num2, num3) {
@@ -36,15 +30,12 @@ function transmogrifier(num1, num2, num3) {
 	var transmogrified = Math.pow(base, num3);
 	return transmogrified;
 };
-
 console.log(transmogrifier(5, 4, 3));
 console.log(transmogrifier(13, 12, 5));
 console.log(transmogrifier(42, 13, 7));
 
 
-
 // ROUND 3
-
 // Write a function called toonify that takes two parameters, accent and sentence.
 function toonify(accent, sentence) {
 	accent.toLowerCase();
@@ -66,15 +57,12 @@ function toonify(accent, sentence) {
 	};
 	return
 };
-
 toonify("daffy", "so you smell like sausage");
 toonify("elmer", "root root for the home team");
 toonify("german", "what are you saying?");
 
 
-
 // ROUND 4
-
 //write function "wordReverse" with single argument, a string
 function wordReverse(string) {
 	//split string by spaces, and reserve order of items to make new string
@@ -82,36 +70,28 @@ function wordReverse(string) {
 	var splitString = string.split(separator).reverse().join(" ");
 	console.log(splitString);
 };
-
 wordReverse("Karly Lynn Hoffman");
 
 
-
 // ROUND 5
-
 //write function "letterReverse" with single argument, a string
 function letterReverse(string) {
 	//split string by letters, and reserve order of items to make new string
 	var lettersReversed = string.split('').reverse().join('');
 	console.log(lettersReversed);
 };
-
 letterReverse("Karly Lynn Hoffman");
 
 
-
 // ROUND 6
-
 //write a function "longest" that accepts a single argument, an array of strings.
 function longest(arrayOfStrings) {
 	//print array
 	console.log(arrayOfStrings);
 	//declare longestString
 	var longestString = '';
-	
 	//pass through the array to find longer string
 	for (var i = 0 ; i < arrayOfStrings.length ; i++) {
-
 		if (arrayOfStrings[i].length > longestString.length ) {
 			longestString = arrayOfStrings[i];
 		} 
@@ -121,13 +101,10 @@ function longest(arrayOfStrings) {
 	}
 	console.log(longestString);
 };
-
 longest(["Oh", "home", "on", "the", "range", "dude"]);
 
 
-
 // FINAL ROUND
-
 //write a function "repMaster" that accepts two arguments, input and a function.
 function repMaster (input, aFunction) {
 	// repMaster should take the result of the string, passed as an argument to the argument function, 
@@ -136,7 +113,6 @@ function repMaster (input, aFunction) {
 	//and return this result with ' proves that I am the rep MASTER!' concatenated to it.
 	console.log(repMasterSentence);
 };
-
 repMaster("woah, I was able to do it!", toonify);
 
 
